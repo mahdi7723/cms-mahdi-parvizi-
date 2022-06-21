bed0df082d0cafda1bda95c3b7ed55faf97f8065
mahdi <mahdiparvizi77@yahoo.com>
2022-06-21 13:34:40 +0430

    create model author and author table and add table in admin

---
+++ database/migrations/2022_06_21_085640_create_authors_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('authors', function (Blueprint $table) {
+            $table->increments('id');
+            $table->unsignedInteger('user_id');
+            $table->timestamps();
+            $table->string('city');
+            $table->enum('Level',['junior,','mid_level','senior']);
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade')->onUpdate('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('authors');
+    }
+};
